29b6b87f3b6e9841571e998df4342a37fcf532d2
Ariana <ariana.garrido@my.unitec.edu.mx>
2022-10-17 12:42:58 -0500

    Unity
    Add UnityFolder

---
+++ VIDEOJUEGO/Assets/Script/MovePlayer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float speed;
+    public bool canJump;
+    public float forceJump;
+    public Transform _initialPosition;
+    public GameObject[] plataforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveController();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Danger"))
+        {
+            transform.position = _initialPosition.position;
+        }
+        if (other.CompareTag("PowerUpJump"))
+        {
+            canJump = true;
+
+            plataforms[0].GetComponent<Rigidbody>().useGravity = true;
+            plataforms[0].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            plataforms[1].GetComponent<Rigidbody>().useGravity = true;
+            plataforms[1].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+            Destroy(other.gameObject);
+        }
+    }
+    void MoveController()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(0, 0, moveVertical * speed * Time.deltaTime);
+        transform.Rotate(0, moveHorizontal, 0 * speed * Time.deltaTime);
+        if (canJump)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidbody.AddForce(Vector3.up * forceJump, ForceMode.Impulse);
+            }
+        }
+    }
+}
